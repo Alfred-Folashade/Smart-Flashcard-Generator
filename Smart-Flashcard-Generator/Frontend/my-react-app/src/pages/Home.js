c45a3069fb2a56db71a60b01801aad6cd0ce5192
Alfred-Folashade <129620082+Alfred-Folashade@users.noreply.github.com>
2025-03-26 11:43:10 +0000

    Moved html code from flashcard_home to home.js

--- Smart-Flashcard-Generator/Frontend/my-react-app/src/pages/Home.js
+++ Smart-Flashcard-Generator/Frontend/my-react-app/src/pages/Home.js
@@ -2,10 +2,25 @@ import React from 'react'
 
 const Home = () => {
     return (
-      <div>
-          
-          <h1>Home</h1>
-      </div>
+        
+        <div>
+            <div className="cloud" style={{top: "50px", left: "50px"}}></div>
+            <div className="cloud" style={{top: "120px", left: "200px"}}></div>
+            <div className="cloud" style={{top: "200px", left: "100px"}}></div>
+            <div className="cloud" style={{top: "300px", left: "250px"}}></div>
+            <div className="cloud" style={{top: "400px", left: "50px"}}></div>
+            <div className="cloud" style={{top: "500px", left: "300px"}}></div>
+            <div className="cloud" style={{top: "600px", left: "150px"}}></div>
+                   
+        
+            <div class="text-box-container">
+                <form action="" method="post">
+                <h2 class="text-box-title">Enter Your Text</h2>
+                <textarea  class="text-input" placeholder="Type your message here..."></textarea>
+                <button class="submit-button">Submit</button>
+                </form>
+            </div>
+        </div>
     )
   }
  
