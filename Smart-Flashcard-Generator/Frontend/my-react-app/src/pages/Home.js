8ab0c91ad63361c880fbc826686e973e6f89ed61
Alfred-Folashade <129620082+Alfred-Folashade@users.noreply.github.com>
2025-04-01 14:47:13 +0100

    Fixed mistakes in home.module.css

--- Smart-Flashcard-Generator/Frontend/my-react-app/src/pages/Home.js
+++ Smart-Flashcard-Generator/Frontend/my-react-app/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import styles from './Home.modules.css'
+import styles from './Home.module.css'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
@@ -36,21 +36,21 @@ const Home = () => {
     
     return (
         
-        <div>
-            <div className="cloud" style={{top: "50px", left: "50px"}}></div>
-            <div className="cloud" style={{top: "120px", left: "200px"}}></div>
-            <div className="cloud" style={{top: "200px", left: "100px"}}></div>
-            <div className="cloud" style={{top: "300px", left: "250px"}}></div>
-            <div className="cloud" style={{top: "400px", left: "50px"}}></div>
-            <div className="cloud" style={{top: "500px", left: "300px"}}></div>
-            <div className="cloud" style={{top: "600px", left: "150px"}}></div>
+        <div className={styles.page}>
+            <div className={styles.cloud} style={{top: "50px", left: "50px"}}></div>
+            <div className={styles.cloud} style={{top: "120px", left: "200px"}}></div>
+            <div className={styles.cloud} style={{top: "200px", left: "100px"}}></div>
+            <div className={styles.cloud} style={{top: "300px", left: "250px"}}></div>
+            <div className={styles.cloud} style={{top: "400px", left: "50px"}}></div>
+            <div className={styles.cloud} style={{top: "500px", left: "300px"}}></div>
+            <div className={styles.cloud} style={{top: "600px", left: "150px"}}></div>
                    
         
-            <div class="text-box-container">
+            <div className={styles.textBoxContainer}>
                 <form onSubmit={handleSubmit} method="post">
-                    <h2 class="text-box-title">Enter Your Text</h2>
-                    <textarea name="text" class="text-input" value={formData.message} onChange={handleChange} placeholder="Type your message here..."></textarea>
-                    <button class="submit-button">Submit</button>
+                    <h2 className={styles.textBoxTitle}>Enter Your Text</h2>
+                    <textarea name="text" className={styles.textInput} value={formData.message} onChange={handleChange} placeholder="Type your message here..."></textarea>
+                    <button className={styles.submitButton}>Submit</button>
                 </form>
             </div>
         </div>
